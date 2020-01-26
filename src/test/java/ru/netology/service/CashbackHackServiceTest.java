76c8fd7ddf969b9efa1ae7b5391bb14d36732b34
NataliaGracheva <40542294+NataliaGracheva@users.noreply.github.com>
2020-01-26 22:19:26 +0500

    @ParametrizedTest added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,35 +1,57 @@
 package ru.netology.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
+//    @Test
+//    void shouldReturn100IfAmountIs900() {
+//        // подготовка данных
+//        CashbackHackService service = new CashbackHackService();
+//        int amount = 900;
+//        int expected = 100;
+//
+//        // выполнение целевого действия
+//        int actual = service.remain(amount);
+//
+//        // сравнение ОР и ФР
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    void shouldReturn0IfAmountIs1000() {
+//        // подготовка данных
+//        CashbackHackService service = new CashbackHackService();
+//        int amount = 1000;
+//        int expected = 0;
+//
+//        // выполнение целевого действия
+//        int actual = service.remain(amount);
+//
+//        // сравнение ОР и ФР
+//        assertEquals(expected, actual);
+//    }
+
     @Test
-    void shouldReturn100IfAmountIs900() {
-        // подготовка данных
+    void shouldReturnExceptionIfAmount0() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 900;
-        int expected = 100;
+        int amount = 0;
 
-        // выполнение целевого действия
-        int actual = service.remain(amount);
-
-        // сравнение ОР и ФР
-        assertEquals(expected, actual);
+        assertThrows(IllegalArgumentException.class,
+                () -> service.remain(amount)
+        );
     }
 
-    @Test
-    void shouldReturn0IfAmountIs1000() {
-        // подготовка данных
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackData.csv", numLinesToSkip = 1)
+    void shouldCalculateRemainAmount(int amount, int expected) {
         CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int expected = 0;
 
-        // выполнение целевого действия
         int actual = service.remain(amount);
 
-        // сравнение ОР и ФР
         assertEquals(expected, actual);
     }
 }
